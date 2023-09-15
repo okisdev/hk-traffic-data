c3a52d9d84bd6ec0e6aaceb28f9628e5024e52bd
Harry Yep <git@hode.co.uk>
2023-09-15 19:31:48 +0800

    feat: generate_folder

---
+++ scripts/file_manager/generate_folder.py
@@ -0,0 +1,13 @@
+import os
+
+# Define the path to the data directory
+data_dir = "data"
+
+# Get a list of all the directories in the data directory
+directories = [
+    d for d in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, d))
+]
+
+# Loop through each directory and create a folder named 'origin'
+for directory in directories:
+    os.makedirs(os.path.join(data_dir, directory, "origin"), exist_ok=True)
